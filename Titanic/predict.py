86f9b558efbad03e2751e7a74d1896ec4171c091
rtty122333 <wangbo7799@gmail.com>
2017-12-24 19:32:12 +0800

    first kaggle submit

---
+++ Titanic/predict.py
@@ -0,0 +1,44 @@
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
+
+train = pd.read_csv(r'data\train.csv')
+test = pd.read_csv(r'data\test.csv')
+
+#print (train.head())
+#print (test.head())
+
+print ("Train size : %d" % len(train))
+print ("Train Survived size : %d" % len(train[train['Survived']==1]))
+
+print ("% of men who survived", 100*np.mean(train['Survived'][train['Sex']=='male']))
+print ("% of women who survived", 100*np.mean(train['Survived'][train['Sex']=='female']))
+
+train['Sex'] = train['Sex'].apply(lambda x: 1 if x =='male' else 0)
+train['Age'] = train['Age'].fillna(np.mean(train['Age']))
+train['Fare'] = train['Fare'].fillna(np.mean(train['Fare']))
+
+test['Sex'] = test['Sex'].apply(lambda x: 1 if x =='male' else 0)
+test['Age'] = train['Age'].fillna(np.mean(test['Age']))
+test['Fare'] = test['Fare'].fillna(np.mean(test['Fare']))
+
+train = train[['Survived','Pclass','Sex','Age','SibSp','Parch','Fare']]
+
+X = train.drop('Survived', axis = 1)
+y = train['Survived']
+
+fix_test = test[['Pclass','Sex','Age','SibSp','Parch','Fare']]
+
+X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.33, random_state=42)
+
+classifier = DecisionTreeClassifier(max_depth=3)
+classifier.fit(X_train, y_train)
+
+print ("Training accuracy :",accuracy_score(y_train,classifier.predict(X_train)))
+print ("Validation accuracy :",accuracy_score(y_test,classifier.predict(X_test)))
+
+result = pd.DataFrame({"PassengerId": test["PassengerId"],"Survived":classifier.predict(fix_test)})
+result.to_csv(r'data\result.csv', index=False)
+
