06e5ba5373aad73f00e7d14cf0a675e75463cefb
rtty122333 <wangbo7799@126.com>
2018-03-22 11:09:18 +0800

    handle cloud text

--- Others/handle_gitlog.py
+++ Others/handle_gitlog.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import io  
 import sys 
+import string
 
 keyword = "x86_64"
 
@@ -15,6 +16,7 @@ class loginst:
             self.author = ''
             self.content = ''
             self.tem_section = ''
+            self.translator = str.maketrans('', '', string.punctuation)
         except:
             print('Open File Error! {}'.format(logname))
             
@@ -23,7 +25,7 @@ class loginst:
         while line:
             if line.find("commit") == 0 :
                 if self.flag == 3:
-                    self.filtertofile(self.commit, self.date, self.author, self.content)
+                    self.transform(self.commit, self.date, self.author, self.content)
                     self.clearvar()
                     self.flag = 0
                 self.commit = line
@@ -35,9 +37,10 @@ class loginst:
                 self.date = line
                 self.flag = 3
             else:
+                line = line.strip('\n')
                 self.content += line
             line = self.files.readline()
-        self.filtertofile(self.commit, self.date, self.author, self.content)
+        self.transform(self.commit, self.date, self.author, self.content)
         self.files.close()
         self.outfile.close()
 
@@ -57,14 +60,63 @@ class loginst:
         self.content = ''
 
     def transform(self, commit, date, author, content):
-        pass
+        commit=commit.strip('\n') 
+        templine = commit[7:]
+        templine += '###'
+        author = author[7:]
+        templine += ','.join(str.split(author))
+        templine += '###'
+        date = date[7:]
+        templine += ','.join(str.split(date))
+        templine += '###'
+        content=content.strip('\n')
+        templine += ','.join(str.split(content.translate(self.translator)))
+        self.outfile.write(templine+'\n')
+        
+    def showtextpic(self,flag):
+        import matplotlib.pyplot as plt
+        import wordcloud
+        cloudtxt = ''
+        line = self.files.readline()
+        while line:
+            if line.find("commit") == 0 :
+                if self.flag == 3:
+#                    self.filtertofile(self.commit, self.date, self.author, self.content)
+                    if flag == 'content':
+                        cloudtxt += self.content
+                    elif flag == 'author':
+                        cloudtxt += self.author
+                    self.clearvar()
+                    self.flag = 0
+                self.commit = line
+                self.flag = 1
+            elif line.find("Author") == 0 :
+                self.author = line
+                self.flag = 2
+            elif line.find("Date") == 0 :
+                self.date = line
+                self.flag = 3
+            else:
+                line = line.strip('\n')
+                self.content += line
+            line = self.files.readline()
+        if flag == 'content':
+            cloudtxt += self.content
+        elif flag == 'author':
+            cloudtxt += self.author
+        cloud = wordcloud.WordCloud(background_color="white",width=2000, height=860, margin=2).generate(cloudtxt)
+        plt.imshow(cloud)
+        plt.axis("off")
+        plt.show()
+        self.files.close()
+        self.outfile.close()
         
         
 def main():
 #    train = pd.read_csv("./share/log.txt");
 #    print(train.describe())
     logcls = loginst("./share/log.txt","out.txt")
-    logcls.handle()
+    logcls.showtextpic('author')
 
 if __name__ == '__main__':
     #改变标准输出的默认编码 
